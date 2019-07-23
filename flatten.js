a007b71326d2684ba86066aa89e52c469ce8d017
Andrew Kim <wk@ualberta.ca>
2019-07-23 22:19:18 +0000

    added flatten.js where there is a flatten function that removes all the nested arrays within an array

--- flatten.js
+++ flatten.js
@@ -39,43 +39,24 @@ const assertArraysEqual = function(arr1, arr2) {
   console.log(result);
 };
 
-const included = function(arr1, arr2) {
-  // input : two arrays
-  // output : an array of booleans indicating if an element in arr1 is included in arr2
+const flatten = function(array) {
+  //input: an array that contain nested array
+  // output: an array that removes the nested array
+
   let outputArray = [];
 
-  //For each element in arr1, check to see if they are in arr2
-  // If the element in arr1 is in arr2, do not added that element into the output array
-  for (let i = 0; i < arr1.length; i++) {
-    let includes = false;
-    if (arr2.includes(arr1[i])) {
-      includes = true;
+  for (let i = 0; i < array.length; i++) {
+    if (!Array.isArray(array[i])) {
+      outputArray.push(array[i]);
+    } else {
+      for (let j = 0; j < array[i].length; j++) {
+        outputArray.push(array[i][j]);
+      }
     }
-    outputArray.push(includes);
   }
 
   return outputArray;
+
 };
 
-const without = function(arr1, arr2) {
-  // input :  two arrays
-  // output:  an altered array that removes all elements that are in arr2 from arr1
-
-  let includedArray = included(arr1, arr2);
-  let outputArray = [];
-
-  for (let i = 0; i < includedArray.length; i++) {
-    if (includedArray[i] === false) {
-      outputArray.push(arr1[i]);
-    }
-  }
-  
-  return outputArray;
-};
-
-console.log(without([1,2,3,1], [1,2]));
-console.log(without([1,2,3],[1]));
-
-let words = ["hello", "world", "lighthouse"];
-console.log(without(["hello", "world", "lighthouse"], ["lighthouse", "hello"]));
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
